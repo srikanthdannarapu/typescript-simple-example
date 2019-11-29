04f37a91129c9e52f209091a6c31f910111b2cfa
Srikanth Dannarapu <srikant.dannarapu@gmail.com>
2019-11-29 08:07:32 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # typescript-simple-example
+- `npm init`
+
+- `tsc --init -g`
+
+- `npm install lodash --save`
+
+- `npm install @types/lodash --save-dev`
---
+++ typescript/person.ts
@@ -0,0 +1,8 @@
+export class Person {
+    firstName : String;
+    lastName : String;
+    constructor(firstName : String, lastName : String) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
